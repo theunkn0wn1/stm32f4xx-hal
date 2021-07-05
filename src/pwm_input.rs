063265c3ad03e168d4e922f8c4194000877febbe
Joshua Salzedo <joshuasalzedo@gmail.com>
2021-07-05 16:26:26 -0700

    Drag the timers in within the proc-macro to avoid feature-gate duplicity

--- src/pwm_input.rs
+++ src/pwm_input.rs
@@ -1,78 +1,12 @@
 use crate::timer::PinC1;
-
-#[cfg(any(
-    feature = "stm32f401",
-    feature = "stm32f405",
-    feature = "stm32f407",
-    feature = "stm32f410",
-    feature = "stm32f411",
-    feature = "stm32f412",
-    feature = "stm32f413",
-    feature = "stm32f415",
-    feature = "stm32f417",
-    feature = "stm32f423",
-    feature = "stm32f427",
-    feature = "stm32f429",
-    feature = "stm32f437",
-    feature = "stm32f439",
-    feature = "stm32f446",
-    feature = "stm32f469",
-    feature = "stm32f479"
-))]
-use crate::stm32::{TIM1, TIM11, TIM5, TIM9};
-use crate::{bb, hal, rcc::Clocks, stm32::RCC, time::Hertz};
-
-#[cfg(any(
-    feature = "stm32f401",
-    feature = "stm32f405",
-    feature = "stm32f407",
-    feature = "stm32f411",
-    feature = "stm32f412",
-    feature = "stm32f413",
-    feature = "stm32f415",
-    feature = "stm32f417",
-    feature = "stm32f423",
-    feature = "stm32f427",
-    feature = "stm32f429",
-    feature = "stm32f437",
-    feature = "stm32f439",
-    feature = "stm32f446",
-    feature = "stm32f469",
-    feature = "stm32f479"
-))]
-use crate::stm32::{TIM10, TIM2, TIM3, TIM4};
-
-#[cfg(any(
-    feature = "stm32f405",
-    feature = "stm32f407",
-    feature = "stm32f412",
-    feature = "stm32f413",
-    feature = "stm32f415",
-    feature = "stm32f417",
-    feature = "stm32f423",
-    feature = "stm32f427",
-    feature = "stm32f429",
-    feature = "stm32f437",
-    feature = "stm32f439",
-    feature = "stm32f446",
-    feature = "stm32f469",
-    feature = "stm32f479"
-))]
-use crate::stm32::{TIM12, TIM13, TIM14, TIM8};
-
-pub struct PwmInput<TIM, PINS> {
-    tim: TIM,
-    pins: PINS,
-}
-
-pub trait Pins<TIM> {}
-
-// implement the `Pins` trait wherever PC1 implements PinC1 and PC2 implements PinC2 for the given TIMer
-impl<TIM, PC1> Pins<TIM> for PC1 where PC1: PinC1<TIM> {}
-
 macro_rules! hal {
     ($($TIM:ident: ($tim:ident, $en_bit:expr, $reset_bit:expr, $apbenr:ident, $apbrstr:ident, $bits:ident),)+) => {
         $(
+            // Drag the associated TIM object into scope.
+            // Note: its drawn in via the macro to avoid duplicating the feature gate
+            // this macro is expecting to be guarded by.
+            use crate::stm32::$TIM;
+
             impl<PINS> PwmInput<$TIM, PINS> {
                 /// Configures a TIM peripheral as a quadrature encoder interface input
                 pub fn $tim(tim: $TIM, pins: PINS) -> Self
@@ -162,6 +96,82 @@ macro_rules! hal {
 }}
 
 #[cfg(any(
+    feature = "stm32f401",
+    feature = "stm32f405",
+    feature = "stm32f407",
+    feature = "stm32f410",
+    feature = "stm32f411",
+    feature = "stm32f412",
+    feature = "stm32f413",
+    feature = "stm32f415",
+    feature = "stm32f417",
+    feature = "stm32f423",
+    feature = "stm32f427",
+    feature = "stm32f429",
+    feature = "stm32f437",
+    feature = "stm32f439",
+    feature = "stm32f446",
+    feature = "stm32f469",
+    feature = "stm32f479"
+))]
+hal! {
+    TIM9: (tim9, 16, 16, apb2enr, apb2rstr, u16),
+    TIM11: (TIM11, 11,11, apb2enr, apb2rstr, u16),
+}
+
+
+use crate::{bb, hal, rcc::Clocks, stm32::RCC, time::Hertz};
+
+#[cfg(any(
+    feature = "stm32f401",
+    feature = "stm32f405",
+    feature = "stm32f407",
+    feature = "stm32f411",
+    feature = "stm32f412",
+    feature = "stm32f413",
+    feature = "stm32f415",
+    feature = "stm32f417",
+    feature = "stm32f423",
+    feature = "stm32f427",
+    feature = "stm32f429",
+    feature = "stm32f437",
+    feature = "stm32f439",
+    feature = "stm32f446",
+    feature = "stm32f469",
+    feature = "stm32f479"
+))]
+use crate::stm32::{TIM10, TIM2, TIM3, TIM4};
+
+#[cfg(any(
+    feature = "stm32f405",
+    feature = "stm32f407",
+    feature = "stm32f412",
+    feature = "stm32f413",
+    feature = "stm32f415",
+    feature = "stm32f417",
+    feature = "stm32f423",
+    feature = "stm32f427",
+    feature = "stm32f429",
+    feature = "stm32f437",
+    feature = "stm32f439",
+    feature = "stm32f446",
+    feature = "stm32f469",
+    feature = "stm32f479"
+))]
+use crate::stm32::{TIM12, TIM13, TIM14, TIM8};
+
+pub struct PwmInput<TIM, PINS> {
+    tim: TIM,
+    pins: PINS,
+}
+
+pub trait Pins<TIM> {}
+
+// implement the `Pins` trait wherever PC1 implements PinC1 and PC2 implements PinC2 for the given TIMer
+impl<TIM, PC1> Pins<TIM> for PC1 where PC1: PinC1<TIM> {}
+
+
+#[cfg(any(
     feature = "stm32f405",
     feature = "stm32f407",
     feature = "stm32f412",
@@ -204,3 +214,5 @@ hal! {
     TIM1: (tim1, 0, 0, apb2enr, apb2rstr, u16),
     TIM5: (tim5, 3, 3, apb1enr, apb1rstr, u32),
 }
+
+
